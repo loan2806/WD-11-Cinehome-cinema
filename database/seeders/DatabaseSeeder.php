e81fa184519ca46bc86859a7cca59170cfced059
Vu Bao <vubao12gksc24@gmail.com>
2026-05-31 22:05:50 +0700

    Add staff account seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AccountSeeder::class,
             TicketSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
